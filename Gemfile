9878cae2949df7755d24b8de8e3964b5c77d2075
Lauri Jutila <lauri@kiskolabs.com>
2012-01-16 15:31:08 +0200

    Updates Gemfile, spec_helper, moves database.yml to example file.

--- Gemfile
+++ Gemfile
@@ -10,13 +10,26 @@ group :assets do
   gem "sass-rails",   "~> 3.1.5"
   gem "coffee-rails", "~> 3.1.1"
   gem "uglifier",     ">= 1.0.3"
+  gem "haml_coffee_assets"
+  gem "execjs"
 end
 
 gem "jquery-rails"
+gem "rails-backbone"
 gem "haml-rails"
+
+gem "devise"
+gem "simple_form"
+gem "state_machine"
 
 gem "rspec-rails", group: [ :development, :test ]
 
 group :test do
+  gem "spork", "> 0.9.0.rc"
+  gem 'rb-fsevent', require: false if RUBY_PLATFORM =~ /darwin/i
+  gem "capybara"
+  gem "factory_girl_rails", "1.4.0", require: false
+  gem "shoulda-matchers"
+  gem "database_cleaner"
   gem "turn", "~> 0.8.3", require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,32 +1,45 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require "capybara/rspec"
+  require "database_cleaner"
 
-RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  RSpec.configure do |config|
+    config.mock_with :rspec
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
 
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
+    # config.before(:suite) do
+    #   DatabaseCleaner.strategy = :transaction
+    #   DatabaseCleaner.clean_with(:truncation)
+    # end
+    # 
+    # config.before(:each) do
+    #   DatabaseCleaner.start
+    # end
+    # 
+    # config.after(:each) do
+    #   DatabaseCleaner.clean
+    # end
+  end
+  
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+  ActiveSupport::Dependencies.clear
+end
+
+Spork.each_run do
+  require "factory_girl_rails"
+  FactoryGirl.factories.clear
+  FactoryGirl.reload
 end
