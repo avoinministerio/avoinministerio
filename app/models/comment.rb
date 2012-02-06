75720f5c80338bee4b353106002e8fe6b8a928fa
Lauri Jutila <lauri@kiskolabs.com>
2012-02-06 11:50:17 +0200

    Ajaxifies comment posting.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, class_name: "Citizen", foreign_key: "author_id"
   belongs_to :commentable, polymorphic: true
+  
+  default_scope order("created_at DESC")
 end
