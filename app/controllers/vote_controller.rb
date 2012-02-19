3cf4c8b607da99ab64a5dc4f9a9b49643f9c5b45
aleksi <aleksi.rossi@iki.fi>
2012-02-19 16:05:54 +0200

    Pie chart of voting displays now correctly. Voting fixed for new Ideas.

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,11 +1,9 @@
 class VoteController < ApplicationController
   def vote(option)
     idea = Idea.find(params[:idea])
-    vote = Vote.new(idea: idea, citizen: current_citizen, option: option)
-    vote = Vote.find_or_create_by_idea_id_and_citizen_id(idea: idea, citizen: current_citizen)
+    vote = Vote.find_or_create_by_idea_id_and_citizen_id(idea.id, current_citizen.id)
     if vote.option == option
-      # no update, no need to save either
-      puts "no update, no need to save either"
+      # logger.info "no update, no need to save either"
     else
       vote.option = option
       flash[:notice] = I18n.t("votes.created") if vote.save
