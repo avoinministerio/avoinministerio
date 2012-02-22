6d8376390f4ffd0dfec7aaaa4309c8581a7fa2c5
Lauri Jutila <lauri@kiskolabs.com>
2012-02-22 17:51:20 +0200

    Turns Idea state image selection to a helper.

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,11 @@
 module IdeasHelper
+  def idea_state_image(idea)
+    filename = {
+      idea:     "state_1_idea.jpg",
+      draft:    "state_2_draft.jpg",
+      proposal: "state_3_proposal.jpg"
+    }[idea.state]
+    
+    image_tag(filename, width: 954, height: 62)
+  end
 end
