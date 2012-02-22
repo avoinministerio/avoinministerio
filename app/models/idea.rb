c2a29baba9758eb5e8f4606cc79be724e63755cc
Lauri Jutila <lauri@kiskolabs.com>
2012-02-22 17:30:37 +0200

    Refactors Idea voting.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,15 @@
 class Idea < ActiveRecord::Base
   has_many :comments, as: :commentable
+  has_many :votes
   
   belongs_to :author, class_name: "Citizen", foreign_key: "author_id"
+  
+  def vote(citizen, option)
+    vote = votes.by(citizen)
+    if vote
+      vote.update_attribute(:option, option) unless vote.option == option
+    else
+      votes.create(citizen: citizen, option: option)
+    end
+  end
 end
