c7beb2436da41b301764f9ab2679d149937471b7
aleksi <aleksi.rossi@gmail.com>
2012-02-24 00:34:06 +0200

    Formats comments timestamps, beginnings of importing Articles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, { auto_link: true, tables: true }).render(text).html_safe
   end
   def shorten(text, max_length, cut_characters, ending_sign)
-  	(text.length < (max_length-cut_characters) ? text : text[0,max_length].gsub(/[\s,.\-!?]+\S+$/, "")) + " " + ending_sign
+    (text.length < (max_length-cut_characters) ? text : text[0,max_length].gsub(/[\s,.\-!?]+\S+$/, "")) + " " + ending_sign
+  end
+
+  def finnishTime(time)
+    sprintf("%d.%d.%d %02d:%02d", time.mday, time.month, time.year, time.hour, time.min)
   end
 end
