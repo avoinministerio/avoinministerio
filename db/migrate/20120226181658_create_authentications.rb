594e051fc5c4d4fc5a867f80018d1ad143544044
Arttu Tervo <arttu.tervo@gmail.com>
2012-02-26 23:44:30 +0200

    Added Authentication model.

--- db/migrate/20120226181658_create_authentications.rb
+++ db/migrate/20120226181658_create_authentications.rb
@@ -10,6 +10,7 @@ class CreateAuthentications < ActiveRecord::Migration
 
       t.timestamps
     end
+
     add_index :authentications, [:provider, :uid], :unique => true
     add_index :authentications, :citizen_id
   end
