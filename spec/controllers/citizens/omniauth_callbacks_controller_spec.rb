aa7ffd2989a82d1239544cbb26bc1189c30b3035
Arttu Tervo <arttu.tervo@gmail.com>
2012-02-26 23:51:24 +0200

    Redirect to the previous page when fb-login was successful.

--- spec/controllers/citizens/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/citizens/omniauth_callbacks_controller_spec.rb
@@ -11,5 +11,16 @@ describe Citizens::OmniauthCallbacksController do
       get :facebook
       response.should redirect_to(root_path)
     end
+
+    it "should authenticate successfully and redirect to original page" do
+      @citizen = Factory.create :facebookin_erkki
+
+      request.env['omniauth.auth'] = auth_hash
+      request.env["devise.mapping"] = Devise.mappings[:citizen]
+      request.env['omniauth.origin'] = new_idea_path
+
+      get :facebook
+      response.should redirect_to(new_idea_path)
+    end
   end
 end
