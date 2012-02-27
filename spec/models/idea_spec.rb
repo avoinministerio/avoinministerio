7e8a7b0290a105cd67b1843d6b3e02f782c45b9a
Lauri Jutila <lauri@kiskolabs.com>
2012-02-27 18:47:51 +0200

    Adds factories, a few specs for Idea and Citizen models.

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,4 +2,31 @@ require 'spec_helper'
 
 describe Idea do
   it { should belong_to :author }
+
+  let(:idea)    { Factory(:idea) }
+  let(:citizen) { Factory(:citizen) }
+    
+  describe "#vote(citizen, option)" do
+    it "casts a vote on an idea" do
+      idea.vote(citizen, 1)
+      idea.votes.count.should == 1
+    end
+    
+    it "casts only one vote per idea per citizen" do
+      idea.vote(citizen, 1)
+      idea.votes.by(citizen).count.should == 1
+      idea.vote(citizen, 1)
+      idea.votes.by(citizen).count.should == 1
+      idea.vote(citizen, 0)
+      idea.votes.by(citizen).count.should == 1
+    end
+  end
+  
+  describe "#voted_by?(citizen)" do
+    it "tells whether citizen has voted for the idea or not" do
+      idea.voted_by?(citizen).should be_false
+      idea.vote(citizen, 1)
+      idea.voted_by?(citizen).should be_true
+    end
+  end
 end
