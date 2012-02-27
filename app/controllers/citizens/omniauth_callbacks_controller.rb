5d22dc9f078845c2b145ba84c403d009777f6d45
Lauri Jutila <lauri@kiskolabs.com>
2012-02-27 18:24:13 +0200

    Code styling and naming changes.

--- app/controllers/citizens/omniauth_callbacks_controller.rb
+++ app/controllers/citizens/omniauth_callbacks_controller.rb
@@ -1,18 +1,16 @@
 class Citizens::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    
-    #logger.info "Facebook callback!!"
     omniauth = request.env["omniauth.auth"]
     
     #TODO Handle current_user situation
-    @user = Citizen.find_for_facebook_oauth(omniauth)
+    @citizen = Citizen.find_for_facebook_oauth(omniauth)
 
-    if @user.persisted?
-     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-     sign_in_and_redirect @user, :event => :authentication
+    if @citizen.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Facebook"
+      sign_in_and_redirect @citizen, event: :authentication
     else
-     session["devise.facebook_data"] = request.env["omniauth.auth"]
-     redirect_to new_citizen_registration_url
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_citizen_registration_url
     end
   end
 end
