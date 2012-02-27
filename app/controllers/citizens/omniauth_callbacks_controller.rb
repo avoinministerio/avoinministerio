528aad5c2b8d91480713b892eccabc9d5247e10c
Arttu Tervo <arttu.tervo@gmail.com>
2012-02-27 19:22:30 +0200

    Create a new citizen unless found in Facebook authentication.

--- app/controllers/citizens/omniauth_callbacks_controller.rb
+++ app/controllers/citizens/omniauth_callbacks_controller.rb
@@ -2,6 +2,10 @@ class Citizens::OmniauthCallbacksController < Devise::OmniauthCallbacksControlle
   def facebook
     @citizen = Citizen.find_for_facebook_auth(request.env["omniauth.auth"])
 
+    unless @citizen
+      @citizen = Citizen.build_from_auth_hash(request.env["omniauth.auth"])
+    end
+
     if @citizen.persisted?
      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
      sign_in_and_redirect @citizen, :event => :authentication
