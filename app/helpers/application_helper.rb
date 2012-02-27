596e787844cbf9266b7d1967c9333d657c4b4bc0
aleksi <aleksi.rossi@iki.fi>
2012-02-28 01:42:38 +0200

    Fix shortening to work on multiline strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, { auto_link: true, tables: true }).render(text).html_safe
   end
   def shorten(text, max_length, cut_characters, ending_sign)
-    (text.length < (max_length-cut_characters) ? text : text[0,max_length].gsub(/[\s,.\-!?]+\S+$/, "")) + " " + ending_sign
+    (text.length < (max_length-cut_characters) ? text : text[0,max_length].gsub(/[\s,.\-!?]+\S+\z/, "")) + " " + ending_sign
   end
 
   def finnishTime(time)
