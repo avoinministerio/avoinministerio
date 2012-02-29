85bb324a82cb406dd30765c5db93870e5ef02e78
Lauri Jutila <lauri@kiskolabs.com>
2012-02-29 14:38:59 +0200

    Updates db/schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,29 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120229101345) do
+ActiveRecord::Schema.define(:version => 20120229113917) do
+
+  create_table "administrators", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",                       :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "administrators", ["email"], :name => "index_administrators_on_email", :unique => true
+  add_index "administrators", ["reset_password_token"], :name => "index_administrators_on_reset_password_token", :unique => true
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -67,10 +89,12 @@ ActiveRecord::Schema.define(:version => 20120229101345) do
     t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "publish_state",    :default => "published"
   end
 
   add_index "comments", ["author_id"], :name => "index_comments_on_author_id"
   add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
+  add_index "comments", ["publish_state"], :name => "index_comments_on_publish_state"
 
   create_table "ideas", :force => true do |t|
     t.string   "title"
@@ -80,9 +104,11 @@ ActiveRecord::Schema.define(:version => 20120229101345) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "summary"
+    t.string   "publish_state", :default => "published"
   end
 
   add_index "ideas", ["author_id"], :name => "index_ideas_on_author_id"
+  add_index "ideas", ["publish_state"], :name => "index_ideas_on_publish_state"
 
   create_table "profiles", :force => true do |t|
     t.integer  "citizen_id"
