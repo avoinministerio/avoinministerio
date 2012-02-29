b02a4478f92b355f0a52ecb5d78bbbf8d720151c
Lauri Jutila <lauri@kiskolabs.com>
2012-02-29 15:39:31 +0200

    Refactors publishing actions in controllers to a separate module. Adds Admin::CommentsController.

--- app/controllers/admin/ideas_controller.rb
+++ app/controllers/admin/ideas_controller.rb
@@ -1,34 +1,19 @@
 class Admin::IdeasController < Admin::AdminController
+  include Admin::PublishingActions
+  
   respond_to :html
-  
-  before_filter :get_idea, except: :index
-  
+
   def index
     respond_with @ideas = Idea.all
   end
   
   def show
-    respond_with @idea
-  end
-  
-  def publish
-    @idea.publish!
-    redirect_to [:admin, @idea]
-  end
-  
-  def unpublish
-    @idea.unpublish!
-    redirect_to [:admin, @idea]
-  end
-  
-  def moderate
-    @idea.moderate!
-    redirect_to [:admin, @idea]
+    respond_with resource
   end
   
   private
-  
-  def get_idea
-    @idea = Idea.find(params[:id])
+
+  def resource
+    @idea ||= Idea.find(params[:id])
   end
 end
