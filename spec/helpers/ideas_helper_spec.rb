0e8d857adf5e62ffc7d265724be17979ab6b3160
Lauri Jutila <lauri@kiskolabs.com>
2012-03-01 11:39:43 +0200

    Full coverage for IdeasHelper.

--- spec/helpers/ideas_helper_spec.rb
+++ spec/helpers/ideas_helper_spec.rb
@@ -1,15 +1,35 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the IdeasHelper. For example:
-#
-# describe IdeasHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe IdeasHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:idea) { Factory(:idea) }
+  
+  describe "#idea_state_image" do
+    let(:draft)     { Factory(:idea, state: "draft") }
+    let(:proposal)  { Factory(:idea, state: "proposal") }
+    let(:law)       { Factory(:idea, state: "law") }
+
+    it "returns image tag for given idea and its state" do
+      helper.idea_state_image(idea).should      =~ /state_1_idea.png/
+      helper.idea_state_image(draft).should     =~ /state_2_draft.png/
+      helper.idea_state_image(proposal).should  =~ /state_3_proposal.png/
+      helper.idea_state_image(law).should       =~ /state_4_law.png/
+    end
+  end
+  
+  describe "#vote_in_words" do
+    let(:citizen_0) { Factory(:citizen) }
+    let(:citizen_1) { Factory(:citizen) }
+    
+    before do
+      idea.vote(citizen_0, 0)
+      idea.vote(citizen_1, 1)
+    end
+    
+    it "returns lingual representation of the vote" do
+      helper.vote_in_words(idea, citizen_0).should == "Ei"
+      helper.vote_in_words(idea, citizen_1).should == "Kyllä"
+    end
+  end
 end
