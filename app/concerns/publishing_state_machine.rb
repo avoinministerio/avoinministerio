0ea91748308d20e0befc1e6368077ac0d3736d4e
Lauri Jutila <lauri@kiskolabs.com>
2012-03-02 18:37:45 +0200

    Moves all publish_state related class methods to PublishingStateMachine so that they'll be available for all classes that include the module.

--- app/concerns/publishing_state_machine.rb
+++ app/concerns/publishing_state_machine.rb
@@ -6,14 +6,26 @@ module PublishingStateMachine
       event :publish do
         transition [ :in_moderation, :unpublished ] => :published
       end
-    
+
       event :unpublish do
         transition [ :published ] => :unpublished
       end
-    
+
       event :moderate do
         transition [ :published ] => :in_moderation
-      end    
+      end
+    end
+
+    def self.published
+      where(publish_state: "published")
+    end
+
+    def self.unpublished
+      where(publish_state: "unpublished")
+    end
+
+    def self.in_moderation
+      where(publish_state: "in_moderation")
     end
   end
 end
