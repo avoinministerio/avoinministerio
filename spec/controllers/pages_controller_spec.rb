96a3dce62449c424839c269e0582a9e01ffcc1b1
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-04 22:03:58 +0200

    Test that home page responses successfully.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe PagesController do
+  describe "#home" do
+    it "should be successful" do
+      get :home
+      response.should be_success
+    end
+  end
 end
