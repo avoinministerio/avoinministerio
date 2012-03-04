03ef657d1ebccb13edc35a6525e52d1a543b1032
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-04 03:33:14 +0200

    We need also changer for changelogs.

--- db/migrate/20120304000619_create_changelogs.rb
+++ db/migrate/20120304000619_create_changelogs.rb
@@ -1,6 +1,8 @@
 class CreateChangelogs < ActiveRecord::Migration
   def change
     create_table :changelogs do |t|
+      t.string :changer_type
+      t.integer :changer_id
       t.string :changelogged_type
       t.integer :changelogged_id
       t.string :change_type
@@ -8,6 +10,7 @@ class CreateChangelogs < ActiveRecord::Migration
 
       t.timestamps
     end
+    add_index :changelogs, [:changer_type, :changer_id]
     add_index :changelogs, [:changelogged_type, :changelogged_id]
   end
 end
