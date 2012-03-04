4bae550891f5dd710384ae4a5b1c2047ecb23d6f
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-04 04:57:38 +0200

    Default scope and pagination per_page method for Changelog model.

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -2,6 +2,12 @@ class Changelog < ActiveRecord::Base
   belongs_to :changer, polymorphic: true
   belongs_to :changelogged, polymorphic: true
 
+  default_scope order(created_at: :desc)
+
+  def self.per_page
+    100
+  end
+
   def attribute_changes
     YAML.load read_attribute(:attribute_changes)
   end
