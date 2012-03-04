c82470cb07dce575d1abdfd721b4a43795f7f9f1
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-05 00:20:05 +0200

    Specs for PagesController#home recent ideas section.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -29,12 +29,52 @@ describe PagesController do
 
         get :home
 
-        assigns[:draft_counts].keys.should include(@recent_draft.id)
         assigns[:draft_counts][@recent_draft.id].should == [7.0/11, '64%', 4.0/11, '36%']
-
-        assigns[:draft_counts].keys.should include(voteless_draft.id)
         assigns[:draft_counts][voteless_draft.id].should == [0.0, ' 0%', 0.0, ' 0%']
       end
+    end
+
+    describe "recent ideas section" do
+      before :each do
+        @recent_idea = Factory(:idea, state: 'idea')
+        @recent_draft = Factory(:idea, state: 'draft')
+      end
+
+      it "should include only ideas in state 'idea' to ideas section" do
+        get :home
+        assigns[:recent_ideas].should include(@recent_idea)
+        assigns[:recent_ideas].should_not include(@recent_draft)
+      end
+
+      it "should calculate vote counts for an idea" do
+        # 1 vote for and 7 votes against the idea
+        1.times { Factory(:vote, idea: @recent_idea, option: 1) }
+        7.times { Factory(:vote, idea: @recent_idea, option: 0) }
+
+        # let's also create an idea without votes yet
+        voteless_idea = Factory(:idea, state: 'idea')
+
+        get :home
+
+        assigns[:idea_counts][@recent_idea.id].should == ['12%', '88%', 0, '8']
+        assigns[:idea_counts][voteless_idea.id].should == ['0%', '0%', 0, 0]
+      end
+
+      it "should include comments count" do
+        3.times { Factory(:comment, commentable: @recent_idea) }
+        another_idea = Factory(:idea, state: 'idea')
+        Factory(:comment, commentable: another_idea)
+
+        get :home
+
+        assigns[:idea_counts][@recent_idea.id].should == ['0%', '0%', 3, 0]
+        assigns[:idea_counts][another_idea.id].should == ['0%', '0%', 1, 0]
+      end
+    end
+
+    describe "blog articles section" do
+      it "should show articles with type = 'blog'"
+      it "should show only published blog articles"
     end
   end
 end
