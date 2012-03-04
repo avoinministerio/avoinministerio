cbe939bc270f3a36625a135e7bbc47ff331748d1
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-04 05:29:10 +0200

    Show AR models' changelogs in admin.

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -2,10 +2,8 @@ class Changelog < ActiveRecord::Base
   belongs_to :changer, polymorphic: true
   belongs_to :changelogged, polymorphic: true
 
-  default_scope order(created_at: :desc)
-
   def self.per_page
-    100
+    50
   end
 
   def attribute_changes
