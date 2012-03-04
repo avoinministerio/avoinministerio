9df33044870d125d19d00551bac06922b3e31e27
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-04 03:33:14 +0200

    We need also changer for changelogs.

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,7 +1,6 @@
 class Changelog < ActiveRecord::Base
-  belongs_to :idea, polymorphic: true
-  belongs_to :comment, polymorphic: true
-  belongs_to :article, polymorphic: true
+  belongs_to :changer, polymorphic: true
+  belongs_to :changelogged, polymorphic: true
 
   def attribute_changes
     YAML.load read_attribute(:attribute_changes)
