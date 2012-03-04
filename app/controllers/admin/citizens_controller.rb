e5a142e927896080f70416c9ff496fccd7b73eaa
aleksi <aleksi.rossi@iki.fi>
2012-03-04 17:38:41 +0200

    Fetch the email list of registered users from /admin/kansalaiset.csv

--- app/controllers/admin/citizens_controller.rb
+++ app/controllers/admin/citizens_controller.rb
@@ -4,7 +4,21 @@ class Admin::CitizensController < Admin::AdminController
   before_filter :build_resource, except: [ :index ]
   
   def index
-    respond_with @citizens = Citizen.all
+    @citizens = Citizen.all
+    respond_to do |wants|
+        wants.html
+        wants.csv do
+          csv_string = CSV.generate do |csv|
+            csv << ["email", "firstname", "lastname"]
+            @citizens.each do |citizen|
+              csv << [citizen.email, citizen.first_name, citizen.last_name]
+            end
+          end
+          send_data csv_string, 
+            type: 'text/csv; charset=ISO-8859-1; header=present',
+            disposition: "attachment; filename=citizens-#{Date.today.to_s}.csv"
+        end
+    end
   end
   
   def show
