d65eb0d7159d3560c59745d61d02222fe84e5e0b
Arttu Tervo <arttu.tervo@gmail.com>
2012-03-15 23:01:18 +0200

    Add a describe block for Admin::ChangelogsHelper#short_diff.

--- spec/helpers/admin/changelogs_helper_spec.rb
+++ spec/helpers/admin/changelogs_helper_spec.rb
@@ -3,28 +3,30 @@
 require 'spec_helper'
 
 describe Admin::ChangelogsHelper do
-  it "should shorten the diff for a change" do
-    original = <<-EOS
+  describe "#short_diff" do
+    it "should shorten the diff for a change" do
+      original = <<-EOS
 Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
 nisi ut aliquip ex ea commodo consequat.
 EOS
-    updated = <<-EOS
+      updated = <<-EOS
 Lorem ipsum dolor sit amet, consectetur adipisicing elit,
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
 nisi ut aliquip ex ea commodo consequat.
 EOS
-    change = [original, updated]
+      change = [original, updated]
 
-    chars_around = Admin::ChangelogsHelper::CHARACTERS_AROUND_DIFF
-    helper.short_diff(change).should =~ /&hellip; (.|\n){#{chars_around}}<del class="differ">qua.\nUt en<\/del><ins class="differ">qua.\nLorem ipsum dolor sit amet, consectetur adipisicing elit.\nUt en<\/ins>(.|\n){#{chars_around}} &hellip;/
-  end
+      chars_around = Admin::ChangelogsHelper::CHARACTERS_AROUND_DIFF
+      helper.short_diff(change).should =~ /&hellip; (.|\n){#{chars_around}}<del class="differ">qua.\nUt en<\/del><ins class="differ">qua.\nLorem ipsum dolor sit amet, consectetur adipisicing elit.\nUt en<\/ins>(.|\n){#{chars_around}} &hellip;/
+    end
 
-  it "should not shorten the diff unnecessarily" do
-    change = ['', 'new value']
-    helper.short_diff(change).should == '<ins class="differ">new value</ins>'
+    it "should not shorten the diff unnecessarily" do
+      change = ['', 'new value']
+      helper.short_diff(change).should == '<ins class="differ">new value</ins>'
+    end
   end
 end
