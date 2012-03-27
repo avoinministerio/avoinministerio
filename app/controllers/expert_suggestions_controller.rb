51049f7f90f015692ed77f46f5e89b041188e51f
aleksi <aleksi.rossi@iki.fi>
2012-03-27 13:13:27 +0300

    Fixes according to Arttu's comments, removed extraneous views and controller methods, and tidied up CSS. Also one precense validator.

--- app/controllers/expert_suggestions_controller.rb
+++ app/controllers/expert_suggestions_controller.rb
@@ -1,29 +1,13 @@
 class ExpertSuggestionsController < ApplicationController
-  before_filter :authenticate_citizen! #, except: [ :index, :show ]
+  before_filter :authenticate_citizen!
   
   respond_to :html
-
-  def index
-    @expert_suggestions = ExpertSuggestion.all
-
-    respond_with @ideas
-  end
-
-  def show
-    @expert_suggestion = ExpertSuggestion.find(params[:id])
-
-    respond_with @ideas
-  end
 
   def new
     @idea = Idea.find(params[:idea_id])
     @expert_suggestion = ExpertSuggestion.new
 
     respond_with @ideas
-  end
-
-  def edit
-    @expert_suggestion = ExpertSuggestion.find(params[:id])
   end
 
   def create
@@ -43,24 +27,4 @@ class ExpertSuggestionsController < ApplicationController
     end
   end
 
-  def update
-    @expert_suggestion = ExpertSuggestion.find(params[:id])
-
-    respond_to do |format|
-      if @expert_suggestion.update_attributes(params[:expert_suggestion])
-        format.html { redirect_to @expert_suggestion, notice: 'Expert suggestion was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @expert_suggestion.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @expert_suggestion = ExpertSuggestion.find(params[:id])
-    @expert_suggestion.destroy
-
-    respond_with @ideas
-  end
 end
