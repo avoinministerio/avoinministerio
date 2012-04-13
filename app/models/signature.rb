7598bf96e5a65423459cbb546f7d326c0a51c8ae
aleksi <aleksi.rossi@iki.fi>
2012-04-14 02:38:06 +0300

    First semi-working version for demo purposes. No error handling, and basic case requires one more action.

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,7 +1,7 @@
 class Signature < ActiveRecord::Base
   VALID_STATES = %w(init query returned cancelled rejected)
 
-  attr_accessible :state
+  attr_accessible :state, :fullname, :birth_date, :occupancy_county, :vow, :signing_date
 
   belongs_to  :citizen
   belongs_to  :idea
