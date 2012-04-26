a2c056cb517c25a90538f62a970388d67d60fd1a
Aleksi Rossi <aleksi.rossi@iki.fi>
2012-04-26 21:03:07 +0300

    Adds correct counter updates for commenting and voting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,4 +14,19 @@ class Comment < ActiveRecord::Base
   validates :author_id,         presence: true
   validates :commentable_id,    presence: true
   validates :commentable_type,  presence: true
+
+  after_create :increment_idea_comment_count
+  def increment_idea_comment_count
+    update_idea_comment_count(1)
+  end
+
+  after_destroy :decrement_idea_comment_count
+  def decrement_idea_comment_count
+    update_idea_comment_count(-1)
+  end
+
+  def update_idea_comment_count(incrdecr)
+    self.commentable.update_attributes(comment_count: self.commentable.comment_count + incrdecr) if self.commentable_type == "Idea"
+  end
+
 end
