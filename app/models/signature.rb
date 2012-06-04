c12516ead1f63a79c901a9d21c47ae2c3d8cfc3f
aleksi <aleksi.rossi@iki.fi>
2012-06-04 23:59:26 +0300

    Debugging Signature accessors

--- app/models/signature.rb
+++ app/models/signature.rb
@@ -1,5 +1,5 @@
 class Signature < ActiveRecord::Base
-  VALID_STATES = %w(init query returned cancelled rejected)
+  VALID_STATES = %w(init query returned cancelled rejected)  # TODO: these are invalid at the moment, it's just initial, authenticated, and the last will be saved or error
 
   attr_accessible :state, :firstnames, :lastname, :birth_date, :occupancy_county, :vow, :signing_date, :stamp
 
