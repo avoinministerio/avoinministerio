0eeec1cfedc2ba1f70d180a21f47b05096bab8e1
aleksi <aleksi.rossi@iki.fi>
2012-06-04 17:17:23 +0300

    Fixing migration to correspond VRK spec

--- db/migrate/20120604134535_split_names_in_signature.rb
+++ db/migrate/20120604134535_split_names_in_signature.rb
@@ -2,11 +2,11 @@ class SplitNamesInSignature < ActiveRecord::Migration
   def up
     add_column 		:signatures, :firstnames, :string
     add_column 		:signatures, :lastname, :string
-    remove_column 	:signatures, :fullname, :string
+    remove_column 	:signatures, :fullname
   end
   def down
-    remove_column 	:signatures, :firstnames, :string
-    remove_column 	:signatures, :lastname, :string
-    add_column 		:signatures, :fullname
+    remove_column 	:signatures, :firstnames
+    remove_column 	:signatures, :lastname
+    add_column 		:signatures, :fullname, :string
   end
 end
