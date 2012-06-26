fac3857bd6eb0fcdf083c382c46241919e61d709
aleksi <aleksi.rossi@iki.fi>
2012-06-27 00:23:16 +0300

    Tries to update full name from authentication if last name recognized properly (with tests)

--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Signature do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#guess_names" do
+    let(:signature)  { SignaturesControllerHelpers }
+    it "guess lastname from beginning TUPAS name 'lastname firstname'" do
+      signature.guess_names("Knuth Donald", "", "Knuth").should == ["Donald", "Knuth"]
+    end
+    it "guess lastname from end of TUPAS name 'lastname firstname'" do
+      signature.guess_names("Donald Knuth", "", "Knuth").should == ["Donald", "Knuth"]
+    end
+    it "guess lastname from beginning TUPAS name 'lastname firstname firstname'" do
+      signature.guess_names("Knuth Donald Ervin", "", "Knuth").should == ["Donald Ervin", "Knuth"]
+    end
+    it "guess lastname from end of TUPAS name 'lastname firstname firstname'" do
+      signature.guess_names("Donald Ervin Knuth", "", "Knuth").should == ["Donald Ervin", "Knuth"]
+    end
+    it "guess returns same if lastname doesn't match TUPAS name" do
+      signature.guess_names("Donald Ervin Knuth", "Donald", "Knuthi").should == ["Donald", "Knuthi"]
+      signature.guess_names("Knuth Donald Ervin", "Donald", "Knuthi").should == ["Donald", "Knuthi"]
+    end
+  end  
 end
