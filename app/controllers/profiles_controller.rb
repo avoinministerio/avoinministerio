3a3c40430077934c7fb4dfbf9757f7ecc7cf6f47
Jyrki Vesterinen <sandgtx@gmail.com>
2012-06-28 17:04:33 +0300

    ProfilesController#fetch_objects no longer checks if a citizen is logged in since that job belongs to the authenticate_citizen! filter. In addition, fetch_objects is now a private method. Both changes were requested by Matias.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,7 @@
 class ProfilesController < ApplicationController
   
+  before_filter :authenticate_citizen!
   before_filter :fetch_objects
-  
-  def fetch_objects
-    if current_citizen
-      @citizen = current_citizen
-      @profile = current_citizen.profile
-      @voted_ideas = Vote.by(current_citizen).map {|v| v.idea}
-      @commented_ideas = current_citizen.comments.map do |comment|
-        if comment.commentable_type == "Idea"
-          comment.commentable
-        end
-      end.uniq
-    else
-      redirect_to :back
-    end
-  end
   
   def edit
     
@@ -35,6 +21,19 @@ class ProfilesController < ApplicationController
       sign_in @citizen, :bypass => true
     end
     render "edit"
+  end
+  
+  private
+  
+  def fetch_objects
+    @citizen = current_citizen
+    @profile = current_citizen.profile
+    @voted_ideas = Vote.by(current_citizen).map {|v| v.idea}
+    @commented_ideas = current_citizen.comments.map do |comment|
+      if comment.commentable_type == "Idea"
+        comment.commentable
+      end
+    end.uniq
   end
 
 end
