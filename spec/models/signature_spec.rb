bfa1c25775208afa3b41a74311b8200e06ee37ba
Jaakko Suutarla <jaakko@suutarla.com>
2012-06-28 11:28:16 +0300

    Basic spec for Signature, just checking fields and associations.

--- spec/models/signature_spec.rb
+++ spec/models/signature_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Signature do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Attributes" do
+    it { should be_accessible(:state) }
+    it { should be_accessible(:firstnames) }
+    it { should be_accessible(:lastname) }
+    it { should be_accessible(:birth_date) }
+    it { should be_accessible(:occupancy_county) }
+    it { should be_accessible(:vow) }
+    it { should be_accessible(:signing_date) }
+    it { should be_accessible(:stamp) }
+    it { should be_accessible(:started) }
+
+    describe "Validations" do
+    end
+  end
+
+  describe "Associations" do
+    it { should belong_to(:citizen) }
+    it { should belong_to(:idea) }
+
+    describe "Validations" do
+      it { should validate_presence_of(:citizen_id) }
+      it { should validate_presence_of(:idea_id) }
+    end
+  end
 end
