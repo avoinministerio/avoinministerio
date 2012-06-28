a8083db268834bf41fb4b40e7a3a4c9b47051ba6
Matias Korhonen <matias@kiskolabs.com>
2012-06-28 16:58:32 +0300

    Removes pending notices.

--- spec/helpers/expert_suggestions_helper_spec.rb
+++ spec/helpers/expert_suggestions_helper_spec.rb
@@ -11,5 +11,4 @@ require 'spec_helper'
 #   end
 # end
 describe ExpertSuggestionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/expert_suggestion_spec.rb
+++ spec/models/expert_suggestion_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
 
 describe ExpertSuggestion do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
