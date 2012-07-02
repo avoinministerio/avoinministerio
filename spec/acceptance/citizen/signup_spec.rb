4a39b699e55760c9118ed9b7ac2ada234ac2198c
Jaakko Suutarla <jaakko@suutarla.com>
2012-07-02 13:45:17 +0300

    Fixed failing acceptance spec for signup.

--- spec/acceptance/citizen/signup_spec.rb
+++ spec/acceptance/citizen/signup_spec.rb
@@ -11,13 +11,13 @@ feature "Citizen Signup" do
     fill_in "Sähköposti", :with => "citizen-kane@example.com"
     fill_in "Salasana", :with => "salainensana12345"
     fill_in "Salasanan vahvistus", :with => "salainensana12345"
-    fill_in "Kutsumanimi", :with => "Kane"
+    fill_in "Kutsumanimi", :with => "Cit-Kane"
+    fill_in "Etunimet", :with => "Kane City"
     fill_in "Sukunimi", :with => "Citizen"
     click_button "Rekisteröidy"
 
     should_be_on homepage
     should_have_the_following 'Kirjaudu ulos'
   end
-
 
 end
