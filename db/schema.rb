7d95097400ceab09481a16d6eed495032e4c6502
Jyrki Vesterinen <sandgtx@gmail.com>
2012-07-16 16:31:47 +0300

    Added a test for the situation where a citizen is already authenticated and has already signed the proposal. I also migrated my database, which automatically changed the schema.rb file. (Looks like Aleksi edited it manually or something.)

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,16 @@ ActiveRecord::Schema.define(:version => 20120716090326) do
 
   create_table "administrators", :force => true do |t|
     t.string   "email"
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",        :default => 0
+    t.integer  "failed_attempts",                       :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "password"
@@ -80,11 +80,11 @@ ActiveRecord::Schema.define(:version => 20120716090326) do
 
   create_table "citizens", :force => true do |t|
     t.string   "email"
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                         :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
