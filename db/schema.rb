a3b6fbe9672e4b5d74d135df6fa916011e82f56b
Jyrki Vesterinen <sandgtx@gmail.com>
2012-07-27 10:57:01 +0300

    Fixed: it was possible to sign the same proposal multiple times. Changed session_store to active_record_store that resists session poisoning attacks much better. I also made some fixes to tests.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716090326) do
+ActiveRecord::Schema.define(:version => 20120727060642) do
 
   create_table "administrators", :force => true do |t|
     t.string   "email"
@@ -176,6 +176,16 @@ ActiveRecord::Schema.define(:version => 20120716090326) do
   end
 
   add_index "profiles", ["citizen_id"], :name => "index_profiles_on_citizen_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "signatures", :force => true do |t|
     t.integer  "citizen_id"
