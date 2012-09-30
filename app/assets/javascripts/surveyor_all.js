454c2464f59c1c8ad92bf202e42a2d1bc507f51e
Wasim Akram <mailwasim@gmail.com>
2012-09-30 22:05:13 +0500

    Enabled multiple survey and temporarily Disabled validations

--- app/assets/javascripts/surveyor_all.js
+++ app/assets/javascripts/surveyor_all.js
@@ -7,28 +7,38 @@
 
 $(document).ready(function(){
 
-  var answered_mendatory = function(){
-    for (var i = 1; i <= 13; i++){
-      if ($("input[name='r[" + i + "][answer_id]']") != null){
-        if ($("input[name='r[" + i + "][answer_id]']").val() != ""){
-          return false;
-        }
-      }
-    }
-    return true;
-  };
+  //function unanswered_question(questions){
+    //for (var i in questions){
+      //if ($(questions[i]).val() == null){
+        //return questions[i];
+      //}
+    //}
+    //return null;
+  //}
 
-  var toggleButton = function() {
-    if(  answered_mendatory() ) {
-      $("input[type='submit']").attr({"disabled": false, "class": "accept-btn"});
-    } else {
-      $("input[type='submit']").attr({"disabled": true, "class": "accept-btn-disabled"});
-    }
-  };
+  //var toggleButton = function() {
+    //var unanswered_mandatory_question = unanswered_question(mandatory_questions);
+    //console.log(unanswered_mandatory_question);
+    //if( unanswered_mandatory_question == null ) {
+      //$("input[type='finish']").attr({"disabled": false, "class": "accept-btn"});
+    //} else {
+      //$("input[type='finish']").attr({"disabled": true, "class": "accept-btn-disabled"});
+    //}
+  //};
 
-  for (var i = 1; i <= 13; i++){
-    if ($("input[name='r[" + i + "][answer_id]']") != null){
-      $("input[name='r[" + i + "][answer_id]']").change(toggleButton);
-    }
-  }
+  //var question_numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  //var mandatory_questions = new Array(question_numbers.length);
+  //for (var i in question_numbers){
+    //mandatory_questions[i] = "input[name='r[" + question_numbers[i] + "][answer_id]']";
+    //$(mandatory_questions[i]).change(toggleButton);
+  //}
+  //$("input[type='finish']").attr({"disabled": true, "class": "accept-btn-disabled"});
+
+  //var scrollToUnansweredQuestion = function(){
+    //var unanswered_mandatory_question = unanswered_question(mandatory_questions);
+    //console.log("Clicked")
+    //console.log(unanswered_mandatory_question)
+    //$(unanswered_mandatory_question).scrollTop();
+  //};
+  //$("input[type='finish']").click(scrollToUnansweredQuestion);
 });
