ce5bba47541167176b7e663b7ef6d60b83e1969a
Raj <raj_abc38@yahoo.com>
2012-11-21 03:01:16 +0530

    Added secret_token file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvoinMinisterio::Application.config.secret_token = 'b946225fcd39ea1de49d97f515eff53e3eea635daedc8af994c53aeefcfa1f6626d6b5cfc7477cc36e39fa36264857b930331418c0f2aa33ef33715e157a9adf'
