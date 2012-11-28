86a48e215c13a24abfe7ec053988bd3ed76ab2fb
Legiarr <legiarr@legiarr-HP-620.(none)>
2012-11-28 21:09:01 +0200

    random proposals on home page added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,11 @@
 class PagesController < ApplicationController
 
   def load(state, count)
-    items = Idea.published.where(state: state).order("updated_at DESC").limit(count).includes(:votes).all
+    if state == "proposal"
+      items = Idea.published.find(:all, :include => :votes, :conditions => "state = 'proposal' AND collecting_ended IS NULL", :limit => count, :order => "RANDOM()").sort_by(&:updated_at).reverse
+    else 
+      items = Idea.published.where(state: state).order("updated_at DESC").limit(count).includes(:votes).all
+    end
     item_counts = {}
 
     items.each do |idea|
