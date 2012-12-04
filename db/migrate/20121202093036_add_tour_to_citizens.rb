2aade1483ef4118c14c38ac71c0bc2d2608abd87
Rock Lee <zimilo@code-trick.com>
2012-12-04 17:52:53 +0800

    Fix PG build error, when setting the column default to 0 nor false

--- db/migrate/20121202093036_add_tour_to_citizens.rb
+++ db/migrate/20121202093036_add_tour_to_citizens.rb
@@ -1,6 +1,6 @@
 class AddTourToCitizens < ActiveRecord::Migration
   def change
-	add_column :citizens, :home_tour_ended, :boolean, :default => 0
-  	add_column :citizens, :idea_tour_ender, :boolean, :default => 0
+	add_column :citizens, :home_tour_ended, :boolean, :default => false
+  	add_column :citizens, :idea_tour_ender, :boolean, :default => false
   end
 end
