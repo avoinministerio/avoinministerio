bd055cad6829be10facefc239e69c13d9aa9c4df
Raj <raj_abc38@yahoo.com>
2012-12-22 16:52:08 +0530

    Removed impressionist custom initializer

--- config/initializers/impression.rb
+++
@@ -1,5 +0,0 @@
-# Use this hook to configure impressionist parameters
-Impressionist.setup do |config|
-  # Define ORM. Could be :active_record (default) and :mongo_mapper
-  # config.orm = :active_record
-end
