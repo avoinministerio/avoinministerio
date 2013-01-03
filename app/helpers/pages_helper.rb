ea1f4acb1ffe0a2778072ce7447ed17d76b8940f
Raj <raj_abc38@yahoo.com>
2013-01-04 02:02:01 +0530

    Showing comment/vote counts

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,13 +2,13 @@ module PagesHelper
   def idea_reason(idea)
     case idea.version_reference[0]
     when 0
-      "Got 100 votes more in favor"
+      "Got 100 votes more in favor. Total votes: #{idea.vote_count}"
     when 1
       "Idea state changed from '#{idea.version_reference[1]}' to '#{idea.version_reference[2]}'"
     when 2
       "New Idea created"
     when 3
-      "Idea got a comment recently"
+      "Idea got a comment recently. Total comments: #{idea.comment_count}"
     else
       "Idea was recently in news"
     end
