81670e15fc5ded313b556bbe3793c68debc152dc
Webmaster <webmaster@ubuntu.(none)>
2013-01-11 12:09:21 +0100

    rails 3.2.11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvoinMinisterio::Application.config.secret_token = '185a6e87f842e005bcf7eaa4bbd3824f01d70245e93c2745495a71e1bd147dfa3d6660dde7cc3f890b6cd206136843bd8b160c8fe92aacd347eaf2be93b05c01'
