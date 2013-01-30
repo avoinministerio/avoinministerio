315843af13139bca31a8a202cfb78db0835b455c
Raj <raj_abc38@yahoo.com>
2013-01-31 01:38:42 +0530

    Added tour on idea show page

--- app/assets/javascripts/app-tour.js
+++ app/assets/javascripts/app-tour.js
@@ -56,54 +56,59 @@ var homePageTour = function(){
         title: "Examples for ideas",
         content: "Want to add some idea? Have a look to these examples before adding new idea."
     });
-    
-    tour.start(true);
+    tour.start();
 }
 
 var ideaShowPageTour = function(){
     var tour = new Tour();
     
     tour.addStep({
-        element: "#register",
-        placement: "bottom",
-        title: "Register with us",
-        content: "Register with us and get chance to participate in laws making and give your children a better country to live in."
+        element: ".body",
+        placement: "top",
+        title: "Breif",
+        content: "Breif description on the idea."
     });
     
     tour.addStep({
-        element: "#login",
+        element: ".summary",
         placement: "bottom",
-        title: "Login here",
-        content: "If you already have signed up, login from here"
+        title: "Summary of the idea",
+        content: "This is the summary where the gist of an idea is written."
     });
     
     tour.addStep({
-        element: "#login-fb",
-        placement: "bottom",
-        title: "Login using facebook account",
-        content: "There are more options for login, use your facebook account to log into the website."
-    });
-    
-    tour.addStep({
-        element: "#all-ideas",
+        element: ".jaa-btn",
         placement: "left",
-        title: "See all existing ideas",
-        content: "Have a look to all the existing ideas."
+        title: "Yes, I favor.",
+        content: "Click here to vote if you favor the idea."
     });
     
     tour.addStep({
-        element: "#new-idea",
-        placement: "bottom",
-        title: "Enter new idea",
-        content: "Got an idea which can make system better? This is the place where your idea can take a step to become a law."
+        element: ".ei-btn",
+        placement: "left",
+        title: "No, I don't favor.",
+        content: "Click here to vote againt the idea."
     });
     
-    //tour.addStep({
-    //  element: "#faq",
-    //  placement: "left",
-    //  title: "Having doubts?",
-    //  content: "If you have some doubts and questions, navigate to FAQ page. We have already answered most of your common questions."
-    //});
+    tour.addStep({
+        element: ".comments",
+        placement: "top",
+        title: "Enter the comments.",
+        content: "Enter the comments about the idea here."
+    });
     
+    tour.addStep({
+        element: ".addthis_toolbox",
+        placement: "left",
+        title: "Share on social media.",
+        content: "Share on your social network account from here."
+    });
+    
+    tour.addStep({
+        element: ".date",
+        placement: "bottom",
+        title: "Created on.",
+        content: "This is the Idea creation date, i.e. when the idea was staged on the open ministry."
+    });
     tour.start();
 }
