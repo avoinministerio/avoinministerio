7804f26fabec13434b58b1f495ce356a4fa5228d
Artur_G <invictusru@gmail.com>
2013-03-11 15:19:39 +0400

    Search nearby locations

--- config/initializers/geo_location_config.rb
+++ config/initializers/geo_location_config.rb
@@ -3,7 +3,8 @@ unless GeoLocation == nil
   GeoLocation::use = :hostip
 # Use Max Mind (paid) 
 # For more information visit: www.maxmind.com/app/city 
-# GeoLocation::use = :maxmind GeoLocation::key = ‘YOUR MaxMind.COM LICENSE KEY’ 
+# GeoLocation::use = :maxmind 
+# GeoLocation::key = ‘YOUR MaxMind.COM LICENSE KEY’ 
 # This location will be used while you develop rather than hitting the maxmind.com api 
 # GeoLocation::dev = ‘US,NY,Jamaica,40.676300,-73.775200’ 
 # IP: 24.24.24.24 # Use this IP in development mode (development and testing will give you 127.0.0.1) 
