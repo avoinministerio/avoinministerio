2cdd80780ff6f9e8d39f16f5ab442820cb50dfaa
Artur_G <invictusru@gmail.com>
2013-03-20 10:38:22 +0400

    Sorting ideas + multi-languages support

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,19 @@
 class Language < ActiveRecord::Base
   attr_accessible :name, :full_name
+
+  def self.sorting_options
+    @languages = []
+    self.all.each do |language|
+      @languages << [ language.name.to_sym, language.full_name ]
+    end
+    return @languages
+  end
+
+  def self.list
+    @languages = Array.new
+    self.all.each do |language|
+      @languages << language.name.to_sym
+    end
+    return @languages
+  end
 end
