3328c76194e9c1b6b2a04b6a40bc340209dd2ac4
Ubik <bart.mouse@gmail.com>
2013-04-01 21:49:47 +0200

    fixed seeding

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -2,7 +2,7 @@ class Changelog < ActiveRecord::Base
   belongs_to :changer, polymorphic: true
   belongs_to :changelogged, polymorphic: true
 
-  attr_accessible :changer_type, :changer_id, :changelogged_type, :changelogged_id, :change_type, :attribute_changes
+  attr_accessible :changer_type, :changer, :changer_id, :changelogged_type, :changelogged_id, :change_type, :attribute_changes
 
   def self.per_page
     50
