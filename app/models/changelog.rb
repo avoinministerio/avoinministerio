c1240424449e7b3946c5e428d44be7e6ebae0827
Ubik <bart.mouse@gmail.com>
2013-04-01 19:18:16 +0200

    changelog fixed attr_accessible

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -2,6 +2,8 @@ class Changelog < ActiveRecord::Base
   belongs_to :changer, polymorphic: true
   belongs_to :changelogged, polymorphic: true
 
+  attr_accessible :changer_type, :changer_id, :changelogged_type, :changelogged_id, :change_type, :attribute_changes
+
   def self.per_page
     50
   end
