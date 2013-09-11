c1133c7dcf369da23a8c0fb3e53409a5e8df4d87
Raj <raj_abc38@yahoo.com>
2013-09-11 23:27:05 +0530

    Show tags on idea show for admin

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -157,7 +157,7 @@ class Idea < ActiveRecord::Base
 
   def self.all_tags
     Tag.select("tags.id, tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
-  end
+   end
 
   def self.tag_counts
     Tag.select("tags.id, tags.name, count(taggings.tag_id) as count").where(:is_location => false).joins(:taggings).group("taggings.tag_id, tags.id, tags.name")
